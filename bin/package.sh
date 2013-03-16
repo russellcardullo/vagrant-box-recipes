7431e4014ef90a4bd3f4a8c4cbe9582a0114bfff
Russell Cardullo <russellcardullo@gmail.com>
2013-03-16 10:38:00 -0700

    Add Ubuntu 12.10 box

--- bin/package.sh
+++ bin/package.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cwd=`pwd`
 
+cd $cwd/aws-ubuntu-12.10-chef
+tar cvzf ../boxes/aws-ubuntu-12.10-chef.box ./metadata.json ./Vagrantfile
+
 cd $cwd/aws-ubuntu-12.04-chef
 tar cvzf ../boxes/aws-ubuntu-12.04-chef.box ./metadata.json ./Vagrantfile
 
