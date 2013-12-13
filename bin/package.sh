0f2958d2e9b865af6fc9fdf2c7ebd56989b5dc90
Russell Cardullo <russellcardullo@gmail.com>
2013-12-13 11:29:35 -0800

    Add box for Ubuntu 13.04

--- bin/package.sh
+++ bin/package.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 cwd=`pwd`
 
-cd $cwd/aws-ubuntu-12.10
-tar cvzf ../boxes/aws-ubuntu-12.10.box ./metadata.json ./Vagrantfile
+boxes="aws-ubuntu-10.04 aws-ubuntu-12.04 aws-ubuntu-12.10 aws-ubuntu-13.04"
 
-cd $cwd/aws-ubuntu-12.04
-tar cvzf ../boxes/aws-ubuntu-12.04.box ./metadata.json ./Vagrantfile
-
-cd $cwd/aws-ubuntu-10.04
-tar cvzf ../boxes/aws-ubuntu-10.04.box ./metadata.json ./Vagrantfile
+for box in $boxes
+do
+  cd $cwd/$box
+  tar cvzf ../boxes/$box.box ./metadata.json ./Vagrantfile
+done
